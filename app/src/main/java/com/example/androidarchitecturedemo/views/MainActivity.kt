53e9920c388ee5c8077dcaf2e61732ff282fec57
eunkyoon kim <katalog@gmail.com>
2024-04-22 19:02:31 +0200

    remove unnecessary codes

--- app/src/main/java/com/example/androidarchitecturedemo/views/MainActivity.kt
+++ app/src/main/java/com/example/androidarchitecturedemo/views/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.androidarchitecturedemo.views
 import EarthquakeAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidarchitecturedemo.databinding.ActivityMainBinding
 import com.example.androidarchitecturedemo.viewmodels.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var vm: MainActivityViewModel
-    private lateinit var adapter: EarthquakeAdapter
+    private val adapter = EarthquakeAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +19,6 @@ class MainActivity : AppCompatActivity() {
 
         vm = ViewModelProvider(this)[MainActivityViewModel::class.java]
 
-        binding.recycler.layoutManager = LinearLayoutManager(this)
-        adapter = EarthquakeAdapter()
         binding.recycler.adapter = adapter
 
         vm.eqItems.observe(this) { it ->
